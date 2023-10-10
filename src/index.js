c9884466c62fbae39f566c374040304f3d002c9e
Fezan Muhammad Ali <callmethengcountio@gmail.com>
2023-10-10 22:24:19 +0500

    Some Changes have done in index.js to check differentbeahviour of component

--- src/index.js
+++ src/index.js
@@ -8,5 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxrating={10} />
+    <StarRating size={24} color="red" />
+    <StarRating
+      color="orange"
+      size={35}
+      maxrating={5}
+      messages={["Terrible", "Good", "Okay", "Best", "Amazing"]}
+    />
   </React.StrictMode>
 );
