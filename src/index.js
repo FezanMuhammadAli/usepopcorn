d579aed4f5958f576a04a6ac01d1ee5eae84976a
Fezan Muhammad Ali <callmethengcountio@gmail.com>
2023-10-11 03:39:45 +0500

    index.js is chenged for various tests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMoviesRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxrating={5} color="blue" onSetRating={setMoviesRating} />
+      <p>This Movies Has Been Rated {movieRating} Stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxrating={10} />
-    <StarRating size={24} color="red" />
+    <StarRating size={24} color="blue" defaultRating={4} />
     <StarRating
       color="orange"
       size={35}
       maxrating={5}
       messages={["Terrible", "Good", "Okay", "Best", "Amazing"]}
+      defaultRating={4}
     />
+
+    <Test />
   </React.StrictMode>
 );
