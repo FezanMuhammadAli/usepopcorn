43c8f17fdbd5ec04b2f857661db9fa33dcd12b95
Fezan Muhammad ALi <callmetheengcountio@gmail.com>
2023-10-20 22:38:15 +0500

    previously we were unable to fetch what user has rated but now with updated one it is solved

--- src/StarRating.js
+++ src/StarRating.js
@@ -24,7 +24,7 @@ StarRating.propTypes = {
 export default function StarRating({
   maxrating = 10,
   color = "#fcc419",
-  size = "48",
+  size = 48,
   messages = [],
   defaultRating = 0,
   onSetRating,
@@ -37,16 +37,19 @@ export default function StarRating({
     color,
     fontSize: `${size / 1.5}px`,
   };
+
+  const handleRating = (newRating) => {
+    setRating(newRating);
+    onSetRating(newRating);
+  };
+
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxrating }, (_, i) => (
           <Star
             key={i}
-            onRating={() => {
-              setRating(i + 1);
-              // onSetRating(rating);
-            }}
+            onRating={() => handleRating(i + 1)}
             full={temRating ? temRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => {
               setTempRating(i + 1);
@@ -64,7 +67,6 @@ export default function StarRating({
           ? messages[temRating ? temRating - 1 : rating - 1]
           : temRating || rating || ""}
       </p>
-      {/* <p>{temRating}</p> */}
     </div>
   );
 }
@@ -102,7 +104,7 @@ function Star({ onRating, full, onHoverIn, onHoverOut, color, size }) {
           <path
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="{2}"
+            strokeWidth="2"
             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
           />
         </svg>
@@ -111,12 +113,4 @@ function Star({ onRating, full, onHoverIn, onHoverOut, color, size }) {
   );
 }
 
-/*
-FULL STAR
-
-
-
-EMPTY STAR
-
-
-*/
+export { StarRating };
