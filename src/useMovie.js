2c2b765284fba5c5e5d58fbb001f5ea45129309d
Fezan Muhammad ALi <callmetheengcountio@gmail.com>
2023-10-22 18:31:14 +0500

    converted an Effect into a useMovie custom hook. that increase code readability and reusability of non visual part.

---
+++ src/useMovie.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+const KEY = "950d0f5c";
+
+export function useMovie(query, callBack) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(
+    function () {
+      callBack?.();
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+          if (!res.ok) throw new Error("Something went wrong!");
+          const data = await res.json();
+
+          if (data.Response === "False") throw new Error("Movie Not Found 😩");
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      return function () {
+        controller.abort(); // cleanup
+      };
+    },
+    [query]
+  );
+  return { movies, isLoading, error };
+}
