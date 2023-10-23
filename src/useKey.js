fe3e814935fcf74505c52895057069f151a26447
Fezan Muhammad ALi <callmetheengcountio@gmail.com>
2023-10-23 18:38:07 +0500

    created a custom hook useKey to handle the key events. it is extracted code from App.js

---
+++ src/useKey.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+export function useKey(key, action) {
+  useEffect(
+    function () {
+      function callBack(e) {
+        if (e.code === key.toLowerCase()) {
+          action();
+        }
+      }
+      document.addEventListener("keydown", callBack);
+      return function () {
+        document.removeEventListener("keydown", callBack);
+      };
+    },
+    [action, key]
+  );
+}
